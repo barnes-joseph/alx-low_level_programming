b9451ca3e61692eed19f66069dbc61551ba56974
barnes-joseph <barnsjoseph05@gmail.com>
2022-04-15 23:58:50 +0000

    Added code for printing a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - A function that 
+ * prints a line with its length equal 
+ * to the integer
+ * @n: int type number
+ */
+void print_line(int n)
+{
+if (n <= 0)
+_putchar('\n');
+else{
+while (n--)
+_putchar(95);
+_putchar('\n');
+}
+}
