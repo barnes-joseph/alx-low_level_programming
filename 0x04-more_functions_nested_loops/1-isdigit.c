08b03da0539e5a109c0867285ff8a9eba0656b6d
barnes-joseph <barnsjoseph05@gmail.com>
2022-04-15 22:29:18 +0000

    Added code to check a character is a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - A function that checks
+ * that a character is a digit
+ * @c: int type number
+ * Return: 0 or 1
+ */
+int _isdigit(int c)
+{
+if((c >= 48) && (c <= 57))
+{
+return (1);
+}
+return (0);
+}
