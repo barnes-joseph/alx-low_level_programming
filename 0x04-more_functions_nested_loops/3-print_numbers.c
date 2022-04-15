2410bf40178c4d526eb58efa69b4a2f0c9dac39a
barnes-joseph <barnsjoseph05@gmail.com>
2022-04-15 23:01:43 +0000

    fixed errors

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,13 +2,13 @@
 /**
  * print_numbers - A function that prints
  * from 0 to 9
- * 
+ *
  */
 void print_numbers(void)
 {
 for(int i = 48; i <= 57; i++)
 {
-_putchar(i);    
+_putchar(i);
 }
 _putchar('\n');
 }
