aecdac8c61c5ca47a99ed022b87c026cb66761ef
barnes-joseph <barnsjoseph05@gmail.com>
2022-04-16 00:43:35 +0000

    Added code for solving fizzbuzz problem

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -  function solves the fizzbuzz problem
+ *
+ */
+void main(void)
+{
+int i;
+char fizz[] = "Fizz";
+char buzz[] = "Buzz";
+char fizzbuzz[] = "FizzBuzz";
+for (i = 1; i <= 100; i++)
+{
+if ((i % 3 == 0) && (i % 5) == 0)
+printf("%s ",fizzbuzz);
+else if (i % 3 == 0)
+printf("%s ",fizz);
+else if (i % 5 == 0)
+printf("%s ",buzz);
+else
+printf("%d ",i);
+}
+printf("\n");
+}
