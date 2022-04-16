5ead72e0beb38ee203da6f127cf76b1cc7577693
barnes-joseph <barnsjoseph05@gmail.com>
2022-04-16 00:53:21 +0000

    Modified function for fizzbuzz problem

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,24 +1,43 @@
+#include "main.h"
 #include <stdio.h>
 /**
- * main -  function solves the fizzbuzz problem
- * Return: 0
+ * main -  program that prints the numbers from 1 to 100,
+ * followed by a new line. But for multiples of three print
+ * Fizz instead of the number and for the multiples of
+ * five print Buzz. For numbers which are multiples of both
+ * three and five print FizzBuzz
+ * Return: 0 or 1
+ *
  */
 int main(void)
 {
-int i;
-char fizz[] = "Fizz";
-char buzz[] = "Buzz";
-char fizzbuzz[] = "FizzBuzz";
-for (i = 1; i <= 100; i++)
+int num = 1;
+while (num <= 100)
 {
-if ((i % 3 == 0) && (i % 5) == 0)
-printf("%s ", fizzbuzz);
-else if (i % 3 == 0)
-printf("%s ", fizz);
-else if (i % 5 == 0)
-printf("%s ", buzz);
+
+if ((num % 3 == 0) & (num % 5 == 0))
+{
+printf("FizzBuzz");
+}
+
+else if (num % 3 == 0)
+{
+printf("Fizz");
+}
+
+else if (num % 5 == 0)
+{
+printf("Buzz");
+}
+
 else
-printf("%d ", i);
+{
+printf("%d", num);
+}
+
+if (num != 100)
+printf(" ");
+num++;
 }
 printf("\n");
 return (0);
