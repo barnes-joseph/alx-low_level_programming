cddf2e2d789b9a1c87820ca2adb9ed7920a8b1f2
barnes-joseph <barnsjoseph05@gmail.com>
2022-04-16 01:26:08 +0000

    test function for print_numbers file

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_number - A function that prints
+ * an integer using _putchar
+ *
+ *
+ */
+void print_number(int n)
+{
+if (n / 10 != 0)
+print_number(n / 10);
+_putchar((n % 10) + '0');
+}
+
+int main(void)
+{
+    print_number(345);
+    return (0);
+}
