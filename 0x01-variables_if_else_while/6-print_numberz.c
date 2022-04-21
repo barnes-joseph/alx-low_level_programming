c49601b9ce2f1390eb0efcb808a7a88e8d8a7ac7
barnes-joseph <barnsjoseph05@gmail.com>
2022-04-21 17:29:09 +0000

    Added function to print all the digits in the base 10 system

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - A function that prints all the digits of base 10
+ *
+ * Return: int
+ */
+int main(void)
+{
+int x = 48;
+while (x <= 57)
+{
+putchar(x);
+x++;
+}
+putchar('\n');
+return (0);
+}
