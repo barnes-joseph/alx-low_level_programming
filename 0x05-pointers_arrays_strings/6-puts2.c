3a27b0888258da7c7ecbfe88ead736988e941d20
barnes-joseph <barnsjoseph05@gmail.com>
2022-04-21 04:49:18 +0000

    Added function to print the characters in a string whose index are even

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts2 - A function that prints the character
+ * whose index is even
+ * @str: String parameter
+ */
+void puts2(char *str)
+{
+int n = 0, i = 0;
+while (str[n])
+n++;
+
+while (n != 1)
+{
+if (i % 2 == 0)
+_putchar(str[i]);
+n--;
+i++;
+}
+_putchar('\n');
+}
