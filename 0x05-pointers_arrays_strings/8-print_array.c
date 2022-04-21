22de90523cbc17c44945823de25482f669144c96
barnes-joseph <barnsjoseph05@gmail.com>
2022-04-21 05:19:29 +0000

    Added function to print a variable number of elements from an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - A function that prints
+ * a number of elements in an array
+ * @a: Array parameter
+ * @n: Number of elements to be printed
+ */
+void print_array(int *a, int n)
+{
+int i = 0;
+int x = n;
+while (x)
+{
+printf(" %d", a[i]);
+if (i == n - 1)
+printf("\n");
+else
+printf(", ");
+i++;
+x--;
+}
+}
