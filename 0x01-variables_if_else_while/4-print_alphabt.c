37ca6e87ffd349da48730d266e2a047370210d1e
barnes-joseph <barnsjoseph05@gmail.com>
2022-04-21 17:20:11 +0000

    Added function to print alphabets except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - A function that prints the alphabets
+ * except q and e
+ * 
+ * Return: int
+ */
+int main(void)
+{
+int x = 97;
+while (x <= 122)
+{
+if ((x == 113) || (x == 101))
+{
+x++;
+continue;
+}
+
+putchar(x);
+x++;
+}
+putchar('\n');
+return (0);
+}
